af4bb656dbd8ff81920199654aecd6fd260c0bab
Grzegorz Pach <grzegorz.andrzej.pach@gmail.com>
2017-01-27 22:40:19 +0100

    level has its own render function

--- js/level.js
+++ js/level.js
@@ -1,5 +1,14 @@
 class Level{
     constructor(level_id){
         this.graphic_path = `../graphics/level${level_id}.png`
+        this.position_y = 0;
+    }
+    render(context){
+        let imageLevel = new Image();
+        imageLevel.addEventListener('load', function(){
+            context.drawImage(imageLevel, 0, imageLevel.level_position_y);
+        })
+        imageLevel.src = this.graphic_path;
+        imageLevel.level_position_y = this.position_y;
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function() {
     var canvas = document.getElementById('paper');
     var context = canvas.getContext('2d');
 
-    let ship = new Ship(20,20);
-    
-    Render.render(context, ship);
+    let ship = new Ship(canvas.width/2,20);
+    let level = new Level('');
+    Render.render(context, ship, level);
 })
