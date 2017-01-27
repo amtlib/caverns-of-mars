384e395d64ab5907730503e78c1086e2ea600f99
Grzegorz Pach <grzegorzpach@iMac-Grzegorz.home>
2017-01-27 20:31:21 +0100

    Add ship class

---
+++ js/ship.js
@@ -0,0 +1,9 @@
+class Ship{
+    constructor(positon_x, position_y){
+        this.positon_x = positon_x;
+        this.position_y = position_y;
+
+        this.graphic_path = 'graphics/ship.png';
+    }
+
+}
